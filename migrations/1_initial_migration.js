8fdeb0edf9451d4b43c29e9517f2dcff340e9e48
Alexander Atallah <alexander.atallah@gmail.com>
2019-09-26 23:02:30 -0700

    update truffle config

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,4 +1,4 @@
-const Migrations = artifacts.require("./Migrations.sol");
+const Migrations = artifacts.require("Migrations");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
