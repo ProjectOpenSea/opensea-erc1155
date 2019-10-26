b3339b444549b10a733a92bc41a43be1324571c6
Alexander Atallah <alexander.atallah@gmail.com>
2019-10-26 15:25:12 -0700

    update sample scripts for custom collectible deployment

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
+  console.log(`Using network: ${network}`);
+  console.log(`Using accounts`, accounts);
   deployer.deploy(Migrations);
 };
