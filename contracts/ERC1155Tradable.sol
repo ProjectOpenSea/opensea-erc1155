2ad4fefa65dac8d1f59d3553cb3ef94c6d3da06b
Globallager <72797635+Globallager@users.noreply.github.com>
2020-10-17 15:21:03 +0800

    Update ERC1155Tradable.sol
    - Fixed typo (721 -> 1155)
    - Removed duplicated import (ERC1155MintBurn already includes ERC1155)

--- contracts/ERC1155Tradable.sol
+++ contracts/ERC1155Tradable.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.5.12;
 
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
-import 'multi-token-standard/contracts/tokens/ERC1155/ERC1155.sol';
 import 'multi-token-standard/contracts/tokens/ERC1155/ERC1155Metadata.sol';
 import 'multi-token-standard/contracts/tokens/ERC1155/ERC1155MintBurn.sol';
 import "./Strings.sol";
@@ -17,7 +16,7 @@ contract ProxyRegistry {
  * ERC1155Tradable - ERC1155 contract that whitelists an operator address, has create and mint functionality, and supports useful standards from OpenZeppelin,
   like _exists(), name(), symbol(), and totalSupply()
  */
-contract ERC1155Tradable is ERC1155, ERC1155MintBurn, ERC1155Metadata, Ownable {
+contract ERC1155Tradable is ERC1155MintBurn, ERC1155Metadata, Ownable {
   using Strings for string;
 
   address proxyRegistryAddress;
@@ -58,7 +57,7 @@ contract ERC1155Tradable is ERC1155, ERC1155MintBurn, ERC1155Metadata, Ownable {
   function uri(
     uint256 _id
   ) public view returns (string memory) {
-    require(_exists(_id), "ERC721Tradable#uri: NONEXISTENT_TOKEN");
+    require(_exists(_id), "ERC1155Tradable#uri: NONEXISTENT_TOKEN");
     return Strings.strConcat(
       baseMetadataURI,
       Strings.uint2str(_id)
